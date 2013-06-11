e85046fb607fdf768af30ad101bb3a3c8a1d07d5
oshan <oliver@topbetta.com>
2013-06-11 12:10:52 +1000

    feature/igas-betting: Final fix on exotic dead heats

--- app/topbetta/api/backend/controllers/RacingController.php
+++ app/topbetta/api/backend/controllers/RacingController.php
@@ -540,7 +540,7 @@ class RacingController extends \BaseController {
 											break;
 										case "E": // Exacta
 											if(!$raceEvent->exacta_dividend == NULL){
-												if(!$raceEvent->exacta_dividend  == serialize($exoticArray) && $raceEvent->exacta_dividend == NULL){
+												if(!$raceEvent->exacta_dividend  == serialize($exoticArray)){
 													// unserialise the existing dividend
 													$previousDivArray = unserialize($raceEvent->exacta_dividend);
 													// add the new dividends
@@ -552,7 +552,7 @@ class RacingController extends \BaseController {
 											break;
 										case "T": // Trifecta
 											if(!$raceEvent->trifecta_dividend == NULL){
-												if(!$raceEvent->trifecta_dividend  == serialize($exoticArray) && $raceEvent->trifecta_dividend == NULL){
+												if(!$raceEvent->trifecta_dividend  == serialize($exoticArray)){
 													// unserialise the existing dividend
 													$previousDivArray = unserialize($raceEvent->trifecta_dividend);
 													// add the new dividends
@@ -564,7 +564,7 @@ class RacingController extends \BaseController {
 											break;
 										case "F": // First Four
 											if(!$raceEvent->firstfour_dividend == NULL){
-												if(!$raceEvent->firstfour_dividend  == serialize($exoticArray) && $raceEvent->firstfour_dividend == NULL){
+												if(!$raceEvent->firstfour_dividend  == serialize($exoticArray)){
 													// unserialise the existing dividend
 													$previousDivArray = unserialize($raceEvent->firstfour_dividend);
 													// add the new dividends
