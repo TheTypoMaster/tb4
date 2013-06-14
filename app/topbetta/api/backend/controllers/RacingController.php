3132aabcf67bd95ff890ca4ba2b381d4d87d27f5
oshan <oliver@topbetta.com>
2013-06-14 10:23:52 +1000

    risa data debugging

--- app/topbetta/api/backend/controllers/RacingController.php
+++ app/topbetta/api/backend/controllers/RacingController.php
@@ -89,8 +89,8 @@ class RacingController extends \BaseController {
 		TopBetta\LogHelper::l("BackAPI: Racing - Reciving POST");
 		
 		// get the JSON POST
-		//$racingJSON = \Input::json();
-		$racingJSON = unserialize(file_get_contents('/tmp/backAPIracing-20130614072624'));
+		$racingJSON = \Input::json();
+		//$racingJSON = unserialize(file_get_contents('/tmp/backAPIracing-20130614072624'));
 		$jsonSerialized = serialize($racingJSON);
 
 		if($this->debug){
@@ -438,6 +438,7 @@ class RacingController extends \BaseController {
 											if(isset($runnerSilkObject->last_starts)){
 												$raceRunner->last_starts = $runnerSilkObject->last_starts;
 											}
+											
 											$o = print_r($runnerSilkObject, true);
 											TopBetta\LogHelper::l("BackAPI: Racing - Processing Runner. Runner Code: $runnerCode, Silk:$raceRunner->silk_id, LastStarts:$raceRunner->last_starts. Object:$o.");
 										}
