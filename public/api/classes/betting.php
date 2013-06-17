8b6ba6e96561fa3f227bf11a03fb9bbe37293bfb
oshan <oliver@topbetta.com>
2013-06-17 11:55:52 +1000

    feature/igas: Exotics debugging

--- public/api/classes/betting.php
+++ public/api/classes/betting.php
@@ -1528,6 +1528,7 @@ class Api_Betting extends JController {
 						}
 
 						foreach ($selections as $selection_id) {
+							file_put_contents('/tmp/saveExoticsBet', "* Exotic bet selection:". $selection_id. "\n", FILE_APPEND | LOCK_EX);
 							$bet->addSelection($runner_list_by_id[$selection_id[$pos]]->number, $position_number);
 						}
 					}
