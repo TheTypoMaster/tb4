44dafdd03acde946bbec450ad79c61ff88cffa2b
oshan <oliver@topbetta.com>
2013-07-02 13:00:27 +1000

    feature/igas: debugging

--- app/topbetta/api/backend/controllers/RacingController.php
+++ app/topbetta/api/backend/controllers/RacingController.php
@@ -556,13 +556,13 @@ class RacingController extends \BaseController {
 								$o = print_r($raceTypeCodeResult,true);
 								
 								TopBetta\LogHelper::l("BackAPI: Racing - Processing Result, Racetype object:$o ", 1);
-								if(is_array($raceTypeCodeResult)){
+								//if(is_array($raceTypeCodeResult)){
 									$meetingTypeCode = $raceTypeCodeResult[0]['type_code'];
 									$meetingCountry = $raceTypeCodeResult[0]['country'];
 									
-								}else{
-									TopBetta\LogHelper::l("BackAPI: Racing - Processing Result, No Meeting Code or Country found. MID:$meetingId, RN:$raceNo, SEL:$selection.", 1);
-								}
+								//}else{
+								//	TopBetta\LogHelper::l("BackAPI: Racing - Processing Result, No Meeting Code or Country found. MID:$meetingId, RN:$raceNo, SEL:$selection.", 1);
+								//}
 					
 								
 								$saveRecord = 0;
