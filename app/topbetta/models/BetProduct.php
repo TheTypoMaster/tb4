77032b26353a07f5a53c62e0f479d9b9bc577068
Oliver Shanahan <oliver@topbetta.com>
2013-07-04 20:14:59 +1000

    feature/igas: debugging

--- app/topbetta/models/BetProduct.php
+++ app/topbetta/models/BetProduct.php
@@ -9,7 +9,22 @@ class BetProduct extends \Eloquent {
     
     public static function isProductUsed($priceType, $betType, $meetingCountry, $meetingGrade, $meetingTypeCode, $providerName){
 
-    	$productID = BetProduct::leftjoin('tb_product_default AS pd', 'pd.tb_product_id', '=', 'tbdb_bet_product.id')
+    	
+    	$productID = "select `tbdb_bet_product.id` from `tbdb_bet_product` 
+    				left join `tb_product_default` as `pd` on `pd`.`tb_product_id` = `tbdb_bet_product.id` 
+    				left join `tb_product_provider_match` as `ppm` on `ppm`.`tb_product_id` = `tbdb_bet_product.id` 
+    				left join `tb_product_provider` as `tpp` on `tpp`.`id` = `ppm.provider_id` 
+    				where `provider_product_name` = '$priceType' 
+    				and `bet_type` = '$betType' 
+    				and `type_code` = '$meetingTypeCode' 
+    				and `country` = '$meetingCountry' 
+    				and `region` = '$meetingGrade' 
+    				and `provider_name` = '$providerName' 
+    				limit 1";
+    	
+    	$result = \DB::select($productID);
+    	
+    	/* $productID = BetProduct::leftjoin('tb_product_default AS pd', 'pd.tb_product_id', '=', 'tbdb_bet_product.id')
     										->leftjoin('tb_product_provider_match AS ppm', 'ppm.tb_product_id', '=', 'tbdb_bet_product.id')
     										->leftjoin('tb_product_provider AS tpp', 'tpp.id', '=', 'ppm.provider_id')
     										->where('provider_product_name', '=', $priceType)
@@ -18,7 +33,7 @@ class BetProduct extends \Eloquent {
     										->where('country', '=', $meetingCountry)
     										->where('region', '=', $meetingGrade)
     										->where('provider_name', '=', $providerName)
-    										->pluck('tbdb_bet_product.id');
-    	return $productID;
+    										->pluck('tbdb_bet_product.id'); */
+    	return $result;
 	}
 }
