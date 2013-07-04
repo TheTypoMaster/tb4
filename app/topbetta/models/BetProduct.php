340a39c30cab19a85d31753e62a6a1861f794444
Oliver Shanahan <oliver@topbetta.com>
2013-07-04 16:56:44 +1000

    feature/igas: fix up id name in query

--- app/topbetta/models/BetProduct.php
+++ app/topbetta/models/BetProduct.php
@@ -10,7 +10,7 @@ class BetProduct extends \Eloquent {
     public static function isProductUsed($priceType, $betType, $meetingCountry, $meetingGrade, $meetingTypeCode, $providerName){
     	 
     	return	BetProduct::join('tb_product_default AS pd', 'pd.id', '=', 'tbdb_bet_product.id')
-    										->join('tb_product_provider_match AS ppm', 'ppm.tb_product_id', '=', 'bp.id')
+    										->join('tb_product_provider_match AS ppm', 'ppm.tb_product_id', '=', 'pd.id')
     										->join('tb_product_provider AS pp', 'pp.id ', '=', 'ppm.provider_id')
     										->where('provider_product_name', '=', $priceType)
     										->where('bet_type', '=', $betType)
