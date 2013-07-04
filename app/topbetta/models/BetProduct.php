2db5070e562803b08f3bea766808e05f2d2be0b2
Oliver Shanahan <oliver@topbetta.com>
2013-07-04 19:07:09 +1000

    feature/igas: still fixn that query

--- app/topbetta/models/BetProduct.php
+++ app/topbetta/models/BetProduct.php
@@ -10,7 +10,7 @@ class BetProduct extends \Eloquent {
     public static function isProductUsed($priceType, $betType, $meetingCountry, $meetingGrade, $meetingTypeCode, $providerName){
     	 
     	return	BetProduct::leftjoin('tb_product_default AS pd', 'pd.tb_product_id', '=', 'tbdb_bet_product.id')
-    										->leftjoin('tb_product_provider_match AS ppm', 'ppm.tb_product_id', '=', 'pd.id')
+    										->leftjoin('tb_product_provider_match AS ppm', 'ppm.tb_product_id', '=', 'tbdb_bet_product.id')
     										->leftjoin('tb_product_provider AS tpp', 'tpp.id', '=', 'ppm.provider_id')
     										->where('provider_product_name', '=', $priceType)
     										->where('bet_type', '=', $betType)
