d3d4aebc1bf86820bb8ac8290852a1801e02fe87
Oliver Shanahan <oliver@topbetta.com>
2013-07-04 16:42:43 +1000

    feature/igas: add igas testing user

--- app/database/seeds/Tb_api_usersTableSeeder.php
+++ app/database/seeds/Tb_api_usersTableSeeder.php
@@ -1,22 +1,26 @@
 <?php
-
 class Tb_api_usersTableSeeder extends Seeder {
-
-    public function run()
-    {
-    	// wipe the table clean before populating
-    	DB::table('tb_api_users')->delete();
-    	
-        $tb_api_users = array(
-        	'username' => 'test_api_user',
-        	'password' => Hash::make('p@ssw0rd!'),
-        	'created_at' => new DateTime,
-        	'updated_at' => new DateTime
-
-        );
-
-        // run the seeder
-        DB::table('tb_api_users')->insert($tb_api_users);
-    }
-
+	public function run() {
+		// wipe the table clean before populating
+		DB::table ( 'tb_api_users' )->delete ();
+		
+		$tb_api_users = array (
+				array (
+						'username' => 'test_api_user',
+						'password' => Hash::make ( 'p@ssw0rd!' ),
+						'created_at' => new DateTime (),
+						'updated_at' => new DateTime () 
+				),
+				array (
+						'username' => 'igas',
+						'password' => Hash::make ( 'igast3st1ng' ),
+						'created_at' => new DateTime (),
+						'updated_at' => new DateTime () 
+				) 
+		)
+		;
+		
+		// run the seeder
+		DB::table ( 'tb_api_users' )->insert ( $tb_api_users );
+	}
 }
