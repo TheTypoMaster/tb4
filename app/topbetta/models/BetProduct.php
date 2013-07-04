7c87884f0213057f984d3fae46c0381a0cc3b3ae
Oliver Shanahan <oliver@topbetta.com>
2013-07-04 17:02:39 +1000

    debugging

--- app/topbetta/models/BetProduct.php
+++ app/topbetta/models/BetProduct.php
@@ -9,9 +9,9 @@ class BetProduct extends \Eloquent {
     
     public static function isProductUsed($priceType, $betType, $meetingCountry, $meetingGrade, $meetingTypeCode, $providerName){
     	 
-    	return	BetProduct::join('tb_product_default AS pd', 'pd.id', '=', 'tbdb_bet_product.id')
-    										->join('tb_product_provider_match AS ppm', 'ppm.tb_product_id', '=', 'pd.id')
-    										->join('tb_product_provider AS tpp', 'tpp.id ', '=', 'ppm.provider_id')
+    	return	BetProduct::leftjoin('tb_product_default AS pd', 'pd.id', '=', 'tbdb_bet_product.id')
+    										->leftjoin('tb_product_provider_match AS ppm', 'ppm.tb_product_id', '=', 'pd.id')
+    										->leftjoin('tb_product_provider AS tpp', 'tpp.id ', '=', 'ppm.provider_id')
     										->where('provider_product_name', '=', $priceType)
     										->where('bet_type', '=', $betType)
     										->where('type_code', '=', $meetingTypeCode)
