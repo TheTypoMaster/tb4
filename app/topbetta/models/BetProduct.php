c836ade9d5feca6c4f3196372c01b8f4c07887da
Oliver Shanahan <oliver@topbetta.com>
2013-07-04 18:13:15 +1000

    feature/igas: Fix query for check products used

--- app/topbetta/models/BetProduct.php
+++ app/topbetta/models/BetProduct.php
@@ -9,16 +9,15 @@ class BetProduct extends \Eloquent {
     
     public static function isProductUsed($priceType, $betType, $meetingCountry, $meetingGrade, $meetingTypeCode, $providerName){
     	 
-    	return	BetProduct::leftjoin('tb_product_default AS pd', 'pd.id', '=', 'tbdb_bet_product.id')
+    	return	BetProduct::leftjoin('tb_product_default AS pd', 'pd.tb_product_id', '=', 'tbdb_bet_product.id')
     										->leftjoin('tb_product_provider_match AS ppm', 'ppm.tb_product_id', '=', 'pd.id')
     										->leftjoin('tb_product_provider AS tpp', 'tpp.id', '=', 'ppm.provider_id')
     										->where('provider_product_name', '=', $priceType)
     										->where('bet_type', '=', $betType)
     										->where('type_code', '=', $meetingTypeCode)
     										->where('country', '=', $meetingCountry)
-    										->where('region', '=', "$meetingGrade")
+    										->where('region', '=', $meetingGrade)
     										->where('provider_name', '=', $providerName)
     										->pluck('tbdb_bet_product.id');
-    
-    }
+	}
 }
