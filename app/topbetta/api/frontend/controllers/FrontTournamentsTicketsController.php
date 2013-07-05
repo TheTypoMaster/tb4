bb93e3b0efc8fbe6d15df67f15c09be999fe6cf4
Michael Costa <fuzzeemic@gmail.com>
2013-07-05 16:43:27 +1000

    Added next to jump for tournaments - needs testing for sports tournaments

--- app/topbetta/api/frontend/controllers/FrontTournamentsTicketsController.php
+++ app/topbetta/api/frontend/controllers/FrontTournamentsTicketsController.php
@@ -10,14 +10,50 @@ class FrontTournamentsTicketsController extends \BaseController {
 		$this -> beforeFilter('auth');
 	}
 
+	public function nextToJump() {
+
+		$userId = \Auth::user() -> id;
+
+		$ticketModel = new \TopBetta\TournamentTicket;
+
+		// active tourn tickets
+		$activeTicketList = $ticketModel -> getTournamentTicketActiveListByUserID($userId);
+
+		$nextToJump = array();
+		
+		if (count($activeTicketList) > 0) {			
+
+			foreach ($activeTicketList as $activeTicket) {
+
+				//get the event group from the tournament id
+				$tournament = \TopBetta\Tournament::find($activeTicket -> tournament_id);
+
+				// get next event for this event group
+				$nextData = \TopBetta\TournamentTicket::nextEventForEventGroup($tournament -> event_group_id);
+
+				$next = (count($nextData) > 0) ? $nextData[0] : false;
+
+				if ($next) {
+
+					$nextToJump[] = array('id' => (int)$next -> id, 'tournament_id' => $activeTicket -> tournament_id, 'type' => $next -> type, 'meeting_id' => (int)$next -> meeting_id, 'meeting_name' => $next -> meeting_name, 'state' => $next -> state, 'race_number' => (int)$next -> number, 'to_go' => \TimeHelper::nicetime(strtotime($next -> start_date), 2), 'start_datetime' => \TimeHelper::isoDate($next -> start_date), 'distance' => $next -> distance);
+
+				}
+			}			
+
+		}
+		
+		return array('success' => true, 'result' => $nextToJump);
+		
+	}
+
 	/**
 	 * Display a listing of the resource.
 	 *
 	 * @return Response
 	 */
 	public function index() {
-			
-		$userId = \Auth::user() -> id;	
+
+		$userId = \Auth::user() -> id;
 
 		$ticketModel = new \TopBetta\TournamentTicket;
 
@@ -27,17 +63,17 @@ class FrontTournamentsTicketsController extends \BaseController {
 		$activeTickets = array();
 
 		foreach ($activeTicketList as $activeTicket) {
-			
+
 			$availableCurrency = $ticketModel -> getAvailableTicketCurrency($activeTicket -> tournament_id, $userId);
 
 			$tournamentModel = new \TopBetta\Tournament;
-			$tournament = $tournamentModel -> find($activeTicket -> tournament_id);	
-			
-			$leaderboardModel = new \TopBetta\TournamentLeaderboard;		
-			$leaderboardDetails = $leaderboardModel->getLeaderBoardRankByUserAndTournament($userId, $tournament);
-			
+			$tournament = $tournamentModel -> find($activeTicket -> tournament_id);
+
+			$leaderboardModel = new \TopBetta\TournamentLeaderboard;
+			$leaderboardDetails = $leaderboardModel -> getLeaderBoardRankByUserAndTournament($userId, $tournament);
+
 			$rank = ($leaderboardDetails -> rank == 0) ? '-' : (int)$leaderboardDetails -> rank;
-			
+
 			$activeTickets[] = array('id' => (int)$activeTicket -> id, 'tournament_id' => (int)$activeTicket -> tournament_id, 'tournament_name' => $activeTicket -> tournament_name, 'buy_in' => (int)$activeTicket -> buy_in, 'entry_fee' => (int)$activeTicket -> entry_fee, 'available_currency' => $availableCurrency, 'turned_over' => (int)$leaderboardDetails -> turned_over, 'leaderboard_rank' => $rank, 'qualified' => ($leaderboardDetails -> qualified) ? true : false, 'sport_name' => $activeTicket -> sport_name, 'start_date' => \TimeHelper::isoDate($activeTicket -> start_date), 'end_date' => \TimeHelper::isoDate($activeTicket -> end_date), 'cancelled_flag' => ($activeTicket -> cancelled_flag) ? true : false);
 
 		}
@@ -49,36 +85,33 @@ class FrontTournamentsTicketsController extends \BaseController {
 		$recentTickets = array();
 
 		foreach ($recentTicketList as $recentTicket) {
-				
+
 			$availableCurrency = $ticketModel -> getAvailableTicketCurrency($activeTicket -> tournament_id, \Auth::user() -> id);
-			
+
 			$tournamentModel = new \TopBetta\Tournament;
 			$tournament = $tournamentModel -> find($activeTicket -> tournament_id);
-			
-			$leaderboardModel = new \TopBetta\TournamentLeaderboard;		
-			$leaderboardDetails = $leaderboardModel->getLeaderBoardRankByUserAndTournament($userId, $tournament);
-			
-			
-			
+
+			$leaderboardModel = new \TopBetta\TournamentLeaderboard;
+			$leaderboardDetails = $leaderboardModel -> getLeaderBoardRankByUserAndTournament($userId, $tournament);
+
 			$prize = 0;
 			if (!$recentTicket -> cancelled_flag && $recentTicket -> result_transaction_id) {
 				if ($recentTicket -> jackpot_flag) {
 
 					$transactionRecord = \TopBetta\FreeCreditBalance::find($recentTicket -> result_transaction_id);
-					
+
 				} else {
 
 					$transactionRecord = \TopBetta\AccountBalance::find($recentTicket -> result_transaction_id);
 				}
 
 				if ($transactionRecord && $transactionRecord -> amount > 0) {
-						
+
 					$prize = $transactionRecord -> amount;
 				}
-			}						
-			
-			
-			$rank = ($leaderboardDetails -> rank == "-") ? 'N/Q' : (int)$leaderboardDetails -> rank;				
+			}
+
+			$rank = ($leaderboardDetails -> rank == "-") ? 'N/Q' : (int)$leaderboardDetails -> rank;
 
 			$recentTickets[] = array('id' => (int)$recentTicket -> id, 'tournament_id' => (int)$recentTicket -> tournament_id, 'tournament_name' => $recentTicket -> tournament_name, 'buy_in' => (int)$recentTicket -> buy_in, 'entry_fee' => (int)$recentTicket -> entry_fee, 'available_currency' => $availableCurrency, 'turned_over' => (int)$leaderboardDetails -> turned_over, 'leaderboard_rank' => $rank, 'prize' => $prize, 'qualified' => ($leaderboardDetails -> qualified) ? true : false, 'sport_name' => $recentTicket -> sport_name, 'start_date' => \TimeHelper::isoDate($recentTicket -> start_date), 'end_date' => \TimeHelper::isoDate($recentTicket -> end_date), 'cancelled_flag' => ($recentTicket -> cancelled_flag) ? true : false);
 
