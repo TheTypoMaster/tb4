92c9bc89e9ffd2fbcdb3f967b00d8c127c450ff4
Michael Costa <fuzzeemic@gmail.com>
2013-07-16 14:29:52 +1000

    Betting history is now using legacy API
    - there is too much legacy specific code to reproduce simply, lots of bet methods required

--- app/topbetta/helpers/LegacyApiHelper.php
+++ app/topbetta/helpers/LegacyApiHelper.php
@@ -2,7 +2,7 @@
 
 class LegacyApiHelper {
 
-	protected $allowed_methods = array('doUserLogin' => 'post', 'doUserRegisterBasic' => 'post', 'doUserRegisterTopBetta' => 'post', 'doInstantDeposit' => 'post', 'doWithdrawRequest' => 'post', 'getLoginHash' => 'post', 'getUser' => 'get', 'saveBet' => 'post', 'saveRacingBet' => 'post', 'saveSportBet' => 'post', 'saveTournamentBet' => 'post', 'saveTournamentSportsBet' => 'post', 'saveTournamentTicket' =>'post', 'setBetLimit' => 'post', 'doSelfExclude' => 'post', 'generateJoomlaPassword' => 'post', 'doReferFriend' => 'post');
+	protected $allowed_methods = array('doUserLogin' => 'post', 'doUserRegisterBasic' => 'post', 'doUserRegisterTopBetta' => 'post', 'doInstantDeposit' => 'post', 'doWithdrawRequest' => 'post', 'getLoginHash' => 'post', 'getUser' => 'get', 'saveBet' => 'post', 'saveRacingBet' => 'post', 'saveSportBet' => 'post', 'saveTournamentBet' => 'post', 'saveTournamentSportsBet' => 'post', 'saveTournamentTicket' =>'post', 'setBetLimit' => 'post', 'doSelfExclude' => 'post', 'generateJoomlaPassword' => 'post', 'doReferFriend' => 'post', 'getBettingHistory' => 'post');
 
 	/*
 	 * @param string $method
@@ -182,7 +182,18 @@ class LegacyApiHelper {
 					$payload[$login_hash['login_hash']] = 1;
 					return $this -> curl('doReferFriend', $this -> allowed_methods['doReferFriend'], $payload);
 
-					break;																				
+					break;	
+
+				case 'getBettingHistory' :
+
+					//1. get login hash
+					$login_hash = $this -> curl('getLoginHash', $this -> allowed_methods['getLoginHash'], $payload, false);
+
+					//2. save bet
+					$payload[$login_hash['login_hash']] = 1;
+					return $this -> curl('getBettingHistory', $this -> allowed_methods['getBettingHistory'], $payload);
+
+					break;																													
 
 				default :
 
