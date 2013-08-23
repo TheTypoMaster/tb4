ebd41b8734794bbecf2fa5d30304da4a52a62147
Oliver Shanahan <oliver@topbetta.com>
2013-08-23 16:06:02 +1000

    sportstype model: test changing the id returned to the markets id from the market types id

--- app/topbetta/models/SportsTypes.php
+++ app/topbetta/models/SportsTypes.php
@@ -8,7 +8,7 @@ class SportsTypes extends \Eloquent {
 
 	public function getTypes($eventId) {
 
-		$query = "SELECT DISTINCT(mt.id) AS id, mt.name AS bet_type, m.market_status as status FROM tbdb_market_type AS mt INNER JOIN tbdb_market AS m ON mt.id = m.market_type_id WHERE m.event_id = $eventId";
+		$query = "SELECT m.id AS id, mt.name AS bet_type, m.market_status as status FROM tbdb_market_type AS mt INNER JOIN tbdb_market AS m ON mt.id = m.market_type_id WHERE m.event_id = $eventId";
 
 		$result = \DB::select($query);
 
