2b05973642987da46543f92926702c4c4510b3d9
Oliver Shanahan <oliver@topbetta.com>
2013-08-26 16:57:11 +1000

    feature/sports_comps_ordering: fix up class names after migrations files were renamed

--- app/database/migrations/2013_08_26_135733_create_tb_data_ordering_order.php
+++ app/database/migrations/2013_08_26_135733_create_tb_data_ordering_order.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTbDataOrderValues extends Migration {
+class CreateTbDataOrderingOrder extends Migration {
 
 	/**
 	 * Run the migrations.
--- app/database/migrations/2013_08_26_135748_create_tb_data_ordering_provider_match.php
+++ app/database/migrations/2013_08_26_135748_create_tb_data_ordering_provider_match.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTbDataOrderOrdering extends Migration {
+class CreateTbDataOrderingProvidermatch extends Migration {
 
 	/**
 	 * Run the migrations.
