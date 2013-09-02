3a760de9ff3f441bb1a84ac4c8cb21a51653712e
Oliver Shanahan <oliver@topbetta.com>
2013-09-02 16:31:52 +1000

    TBL - 66: Remove acc and tourn bals from user interface. Add default date to bet's interface filters

--- public/administrator/components/com_betting/controller.php
+++ public/administrator/components/com_betting/controller.php
@@ -40,8 +40,8 @@ class BettingController extends JController {
 		$filter	= array(
 			'keyword'		=> $filter_keyword,
 			'result_type'	=> $filter_result_type,
-			'from_time'		=> $filter_from_date ? strtotime($filter_from_date) : null,
-			'to_time'		=> $filter_to_date ? (strtotime($filter_to_date) + 24 * 60 * 60) : null,
+			'from_time'		=> $filter_from_date ? strtotime($filter_from_date) : (time() - 24 * 60 * 60),
+			'to_time'		=> $filter_to_date ? (strtotime($filter_to_date) + 24 * 60 * 60) : time(),
 			'from_amount'	=> $filter_from_amount,
 			'to_amount'		=> $filter_to_amount,
 		);
