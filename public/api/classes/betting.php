5401a10a39dc11b5e7b9d387540eae9ea5ced135
Oliver Shanahan <oliver@topbetta.com>
2013-09-06 12:15:00 +1000

    TBL-61 - another user_id not being passed to a get total on the sports betting free credit check.

--- public/api/classes/betting.php
+++ public/api/classes/betting.php
@@ -2126,7 +2126,7 @@ class Api_Betting extends JController {
 				}
 
 				// deduct from account
-				$free_bet_amount = ((int)$free_bet_amount_input > 0) ? $tournamentdollars_model->getTotal() : 0;
+				$free_bet_amount = ((int)$free_bet_amount_input > 0) ? $tournamentdollars_model->getTotal($user->id) : 0;
 				$bet_freebet_transaction_id = $bet_freebet_refund_transaction_id =0;
 
 				if ($debugflag == 1){
