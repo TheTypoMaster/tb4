1c901d85509c2216d15442db1189f54c4680ad7a
Michael Costa <fuzzeemic@gmail.com>
2014-02-11 14:18:45 +1100

    Racing win/place bets sending to risk manager
    - need to test live, can’t test bets dev
    - only sends to risk manager if running on production (checks host)

---
+++ public/api/helpers/riskmanager.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @version		$Id: riskmanager.php  Michael Costa $
+ * @package		
+ * @subpackage	Admin
+ * @copyright	Copyright (C) 2014 TopBetta. All rights reserved.
+ * @license		GNU/GPL, see LICENSE.php
+ * Joomla! is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
+class RiskManagerHelper
+{
+
+    static $riskManagerAPI = 'http://risk.mugbookie.com/api/v1/';
+    static $productionHost = 'services.topbetta.com.au';
+
+    public function sendRacingBet($betData)
+    {
+        // $betData = json_encode($betData);     
+        $response = RiskManagerHelper::curl('bets', 'post', $betData);
+
+        // TODO: add error checking with response		
+
+        return true;
+    }
+
+    private function curl($endPoint, $type, $payload = array())
+    {
+        // we only want to send to risk manager for production
+        if ($_SERVER['HTTP_HOST'] != RiskManagerHelper::$productionHost) {
+            return false;
+        }
+        $url = RiskManagerHelper::$riskManagerAPI . $endPoint;
+
+        $ch = curl_init();
+        if ($type == 'post') {
+            //send through our payload as post fields
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($payload));
+        } else {
+            //send through our payload as query string
+            $url .= '&' . http_build_query($payload);
+        }
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+        $buffer = json_decode(curl_exec($ch), true);
+
+        curl_close($ch);
+
+        return $buffer;
+    }
+
+}
+
+?>
