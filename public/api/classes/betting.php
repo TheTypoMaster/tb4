525f52212b57d476b8ce9d49d6cce7208c7fa29a
Oliver Shanahan <oliver@topbetta.com>
2014-08-06 14:26:45 +1000

    put race betting back to how it was to fix branching issue

--- public/api/classes/betting.php
+++ public/api/classes/betting.php
@@ -987,23 +987,21 @@ class Api_Betting extends JController
             $race_status_model = new TournamentModelEventStatus();
             $selling_status = $race_status_model->getEventStatusByKeywordApi('selling');
 
-            $overRide = $race->override_start;
-
-            if ($race->event_status_id != $selling_status->id && !$overRide) {
+            if ($race->event_status_id != $selling_status->id) {
                 $validation->error = JText::_('Betting was closed');
                 return OutputHelper::json(500, array('error_msg' => $validation->error));
             }
             
             // special case for greyhounds to allow betting after jump time if allowed only
             // all other race types are always closed via the race status only
-//            $pastStartCheck = (time() > strtotime($race->start_date)) ? true : false;
+            $pastStartCheck = (time() > strtotime($race->start_date)) ? true : false;
+            $overRide = $race->override_start;
 
-            
 //            if ($meeting->type_code == "G" && $pastStartCheck && !$overRide) {
-//            if ($pastStartCheck && !$overRide) {
-//                $validation->error = JText::_('Betting was closed');
-//                return OutputHelper::json(500, array('error_msg' => $validation->error));
-//            }
+            if ($pastStartCheck && !$overRide) {
+                $validation->error = JText::_('Betting was closed');
+                return OutputHelper::json(500, array('error_msg' => $validation->error));
+            }
 
             require_once (JPATH_BASE . DS . 'components' . DS . 'com_betting' . DS . 'models' . DS . 'bet.php');
             $bet_model = new BettingModelBet();
@@ -1511,25 +1509,21 @@ class Api_Betting extends JController
             $race_status_model = new TournamentModelEventStatus();
             $selling_status = $race_status_model->getEventStatusByKeywordApi('selling');
 
-            $overRide = $race->override_start;
-
-            if ($race->event_status_id != $selling_status->id && !$overRide) {
+            if ($race->event_status_id != $selling_status->id) {
                 $validation->error = JText::_('Betting was closed');
                 return OutputHelper::json(500, array('error_msg' => $validation->error));
             }
 
-
-
             // special case for greyhounds to allow betting after jump time if allowed only
             // all other race types are always closed via the race status only
-//           $pastStartCheck = (time() > strtotime($race->start_date)) ? true : false;
+            $pastStartCheck = (time() > strtotime($race->start_date)) ? true : false;
+            $overRide = $race->override_start;
 
-            
 //            if ($meeting->type_code == "G" && $pastStartCheck && !$overRide) {
-//            if ($pastStartCheck && !$overRide) {
-//                $validation->error = JText::_('Betting was closed');
-//                return OutputHelper::json(500, array('error_msg' => $validation->error));
-//            }
+            if ($pastStartCheck && !$overRide) {
+                $validation->error = JText::_('Betting was closed');
+                return OutputHelper::json(500, array('error_msg' => $validation->error));
+            }
 
             require_once (JPATH_BASE . DS . 'components' . DS . 'com_betting' . DS . 'models' . DS . 'bet.php');
             $bet_model = new BettingModelBet();
