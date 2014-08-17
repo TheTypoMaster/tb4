ae65ceebb8f13da62bade9b4b6706bf2675e3f6e
Oliver Shanahan <oliver@topbetta.com>
2014-08-17 11:45:49 +1000

    add tournament selection resilt model and repo

---
+++ app/topbetta/repositories/DbTournamentSelectionResultRepository.php
@@ -0,0 +1,33 @@
+<?php namespace TopBetta\Repositories;
+/**
+ * Coded by Oliver Shanahan
+ * File creation date: 16/08/2014
+ * File creation time: 11:58 PM
+ * Project: tb4
+ */
+
+use TopBetta\Models\DbTournamentSelectionResultModel;
+
+class DbTournamentSelectionResultRepository extends BaseEloquentRepository {
+
+    protected $model;
+
+    public function __construct(DbTournamentSelectionResultModel $tournamentbetselectionresults){
+        $this->model = $tournamentbetselectionresults;
+    }
+//
+//    /**
+//     * @param $ticketId
+//     * @return mixed
+//     */
+//    public function getResultedUserBetsInTournament($ticketId){
+//        return $this->model->where('tournament_ticket_id', $ticketId)
+//                            ->get();
+//                           // ->where('bet_result_status_id', $status)->get();
+//    }
+
+    public function getSelectionResultForSelectionId($selectionId){
+        return $this->model->where('selection_id', $selectionId)
+                            ->first();
+    }
+} 
