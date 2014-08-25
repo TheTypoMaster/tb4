e293ec9af37163f50173802c67de55342e005a9f
Oliver Shanahan <oliver@topbetta.com>
2014-08-26 08:30:31 +1000

    no bets after start time

--- public/api/classes/betting.php
+++ public/api/classes/betting.php
@@ -2107,7 +2107,7 @@ class Api_Betting extends JController
 
             $match =  $sportsBetting_model->getEventApi($betMatchID);
 
-            if (strtotime($match->start_date) > time()) {
+            if (strtotime($match->start_date) < time()) {
                 return OutputHelper::json(500, array('error_msg' => JText::_('Match has already started')));
             }
 
