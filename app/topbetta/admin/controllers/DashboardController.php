a1ab6d1093dcd703153b81dc81be76afcb268593
Michael Costa <fuzzeemic@gmail.com>
2014-10-03 10:56:59 +1000

    Fixed total bet amount in admin dashboard
    - also added some caching to queries

--- app/topbetta/admin/controllers/DashboardController.php
+++ app/topbetta/admin/controllers/DashboardController.php
@@ -5,6 +5,7 @@ namespace TopBetta\admin\controllers;
 use Carbon\Carbon;
 use User;
 use View;
+use TopBetta\Bet;
 
 class DashboardController extends \BaseController
 {
@@ -20,28 +21,32 @@ class DashboardController extends \BaseController
 		$today = Carbon::now()->toDateString();
 		$totals = array(
 			'account_balances' => array(
-				'title' => 'Total Account Balances',
-				'amount' => '$' . number_format(\TopBetta\AccountBalance::sum('amount') / 100, 2)
+				'title' => 'Total Account Balances (~15min)',
+				'amount' => '$' . number_format(\TopBetta\AccountBalance::remember(15)->sum('amount') / 100, 2)
 			),
 			'free_credit_balances' => array(
-				'title' => 'Total Free Credit Balances',
-				'amount' => '$' . number_format(\TopBetta\FreeCreditBalance::sum('amount') / 100, 2)
+				'title' => 'Total Free Credit Balances (~15min)',
+				'amount' => '$' . number_format(\TopBetta\FreeCreditBalance::remember(15)->sum('amount') / 100, 2)
 			),			
 			'todays_signups' => array(
 				'title' => 'Registered Today',
-				'amount' => User::where('registerDate', 'LIKE', $today . '%')->count()
+				'amount' => User::remember(1)->where('registerDate', 'LIKE', $today . '%')->count()
 			),			
 			'todays_bets_total' => array(
 				'title' => 'Total Bets Today / (FC)',
-				'amount' => \TopBetta\Bet::where('created_date', 'LIKE', $today . '%')->count() . 
-				' / (' . \TopBetta\Bet::where('created_date', 'LIKE', $today . '%')->where('bet_freebet_flag', 1)->count() . ')'
+				'amount' => Bet::remember(1)->where('created_date', 'LIKE', $today . '%')->count() . 
+				' / (' . Bet::remember(1)->where('created_date', 'LIKE', $today . '%')->where('bet_freebet_flag', 1)->count() . ')'
 			),			
 			'todays_bets_amount' => array(
 				'title' => 'Total Bet Amount Today (inc. FC)',
-				'amount' => '$' . number_format(\TopBetta\Bet::where('created_date', 'LIKE', $today . '%')->sum('bet_amount` + `bet_freebet_amount') / 100, 2)
+				'amount' => '$' . number_format(
+						Bet::remember(1)->where('created_date', 'LIKE', $today . '%')
+						->select(\DB::raw('SUM(`bet_amount` + `bet_freebet_amount`) AS aggregate'))
+						->first()->aggregate
+						/ 100, 2)
 			),			
 		);
-
+		
 		return View::make('admin::dashboard.index')
 						->with(compact('balances', 'totals'));
 	}
