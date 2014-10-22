0829825ae8f57da875c716e7c624565071853bf3
Oliver Shanahan <oliver@topbetta.com>
2014-10-23 10:29:44 +1100

    debugging

--- app/topbetta/models/RaceEvents.php
+++ app/topbetta/models/RaceEvents.php
@@ -91,7 +91,7 @@ class RaceEvent extends \Eloquent {
         return static::join('tbdb_event_group_event', 'tbdb_event.id', '=', 'tbdb_event_group_event.event_id')
             ->join('tbdb_event_group', 'tbdb_event_group.id', '=', 'tbdb_event_group_event.event_group_id')
             ->where('tbdb_event_group.external_event_group_id',$meetingId )
-            ->where('tbdb_event.number',$raceNo)->select('tbdb_event.event_id as EventId', 'tbdb_event.external_event_id as ExternalEventId')
+            ->where('tbdb_event.number',$raceNo)->select('tbdb_event.id as EventId', 'tbdb_event.external_event_id as ExternalEventId')
             ->get()
             ->toArray();
 
