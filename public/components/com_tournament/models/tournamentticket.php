d94ea7e674e469ce3fea23c0764f322c3b9c105a
Oliver Shanahan <oliver@topbetta.com>
2014-11-11 14:39:13 +1100

    typo extra_starting_currency

--- public/components/com_tournament/models/tournamentticket.php
+++ public/components/com_tournament/models/tournamentticket.php
@@ -606,7 +606,7 @@ class TournamentModelTournamentTicket extends JModel
 		$db->setQuery($query);
 
 		$data = $db->loadObject();
-        echo "#### Tournament ID: $tournament_id, UserId:$user_id, Start Currency:{$tournament->start_currency}, Extra:{$ticket->extra_start_currency}\n";
+        echo "#### Tournament ID: $tournament_id, UserId:$user_id, Start Currency:{$tournament->start_currency}, Extra:{$ticket->extra_starting_currency}\n";
 		return $tournament->start_currency + $ticket->extra_starting_currency - $data->bet_amount + $data->win_amount;
 	}
 
