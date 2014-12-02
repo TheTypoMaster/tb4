ded6886ccd495208e8194ba1681e841f1c7f08ff
Oliver Shanahan <oliver@topbetta.com>
2014-12-02 19:50:23 +1100

    add select to event model

--- app/topbetta/repositories/DbEventRepository.php
+++ app/topbetta/repositories/DbEventRepository.php
@@ -50,7 +50,9 @@ class DbEventRepository extends BaseEloquentRepository implements EventRepositor
         return $this->model->join('tbdb_event_group_event', 'tbdb_event.id', '=', 'tbdb_event_group_event.event_id')
                             ->join('tbdb_event_group', 'tbdb_event_group.id', '=', 'tbdb_event_group_event.event_group_id')
                             ->where('tbdb_event_group.external_event_group_id', $meetingId )
-                            ->where('tbdb_event.number', $raceNo)->first();
+                            ->where('tbdb_event.number', $raceNo)
+                            ->select('tbdb_event.*')
+                            ->first();
     }
 
 
