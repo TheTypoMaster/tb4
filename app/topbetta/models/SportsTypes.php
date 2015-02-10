cfa72450134284397b3b02af40e5e14372fed4a5
Thomas Muir <thomas@topbetta.com>
2015-02-10 13:42:50 +1100

    Added check for selection in getTournamentTypes query

--- app/topbetta/models/SportsTypes.php
+++ app/topbetta/models/SportsTypes.php
@@ -37,7 +37,10 @@ class SportsTypes extends \Eloquent {
 					INNER JOIN tbdb_event_group AS eg ON eg.id = egmt.event_group_id
 					INNER JOIN tbdb_event_group_event AS ege ON ege.event_group_id = eg.id
 					INNER JOIN tbdb_selection s ON s.market_id = m.id
+					INNER JOIN tbdb_selection_price sp ON s.id = sp.selection_id
 					WHERE eg.id = '$compId' AND e.id = '$eventId' and m.display_flag = '1'
+					AND sp.win_odds > 1
+					AND s.selection_status_id = '1'
 					AND m.market_status != 'D'
 					GROUP BY id
 					ORDER BY -(ordering) DESC";
