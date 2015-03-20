9dd3d8edc1304f071a2253ef4479ccd1ef287707
Thomas Muir <thomas@topbetta.com>
2015-03-20 12:13:21 +1100

    Updated market type migration to include market rules field

--- app/database/migrations/2015_03_19_141706_add_fields_to_market_type_table.php
+++ app/database/migrations/2015_03_19_141706_add_fields_to_market_type_table.php
@@ -17,6 +17,8 @@ class AddFieldsToMarketTypeTable extends Migration {
 			$table->string('short_name')->after('name');
             $table->string('default_name')->after('short_name');
 
+            $table->text('market_rules')->after('description');
+
             $table->tinyInteger('display_flag')->after('status_flag');
 
             $table->integer('icon_id')->after('display_flag')->unsigned();
@@ -34,6 +36,7 @@ class AddFieldsToMarketTypeTable extends Migration {
 		{
 			$table->dropColumn('short_name');
             $table->dropColumn('default_name');
+            $table->dropColumn('market_rules');
             $table->dropColumn('display_flag');
             $table->dropColumn('icon_id');
 		});
