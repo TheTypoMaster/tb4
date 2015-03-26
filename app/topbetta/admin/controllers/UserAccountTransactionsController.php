f27b37a94ea28f3bf3d844cd78e5235eb426f5f0
Thomas Muir <thomas@topbetta.com>
2015-03-26 14:01:04 +1100

    Created account transaction form in admin section.

--- app/topbetta/admin/controllers/UserAccountTransactionsController.php
+++ app/topbetta/admin/controllers/UserAccountTransactionsController.php
@@ -3,9 +3,14 @@
 namespace TopBetta\admin\controllers;
 
 use BaseController;
+use Carbon\Carbon;
 use TopBetta\Repositories\AccountTransactionRepo;
+use TopBetta\Repositories\Contracts\AccountTransactionRepositoryInterface;
+use TopBetta\Repositories\Contracts\AccountTransactionTypeRepositoryInterface;
 use User;
 use View;
+use Input;
+use Redirect;
 
 class UserAccountTransactionsController extends \BaseController
 {
@@ -19,13 +24,20 @@ class UserAccountTransactionsController extends \BaseController
 	 * @var AccountTransactionRepo
 	 */
 	private $accountTransactionRepo;
+    /**
+     * @var AccountTransactionTypeRepositoryInterface
+     */
+    private $accountTransactionTypeRepository;
 
-	public function __construct(AccountTransactionRepo $accountTransactionRepo, User $user)
+    public function __construct(AccountTransactionRepositoryInterface $accountTransactionRepo,
+                                AccountTransactionTypeRepositoryInterface $accountTransactionTypeRepository,
+                                User $user)
 	{
 
 		$this->accountTransactionRepo = $accountTransactionRepo;
 		$this->user = $user;
-	}
+        $this->accountTransactionTypeRepository = $accountTransactionTypeRepository;
+    }
 
 	/**
 	 * Display a listing of the resource.
@@ -35,12 +47,38 @@ class UserAccountTransactionsController extends \BaseController
 	public function index($userId)
 	{
 		$user = $this->user->find($userId);
-		$transactions = $this->accountTransactionRepo->userTransactions($user->id);
+		$transactions = $this->accountTransactionRepo->getUserTransactionsPaginated($user->id);
 
 		return View::make('admin::transactions.user.index')
 						->with(compact('transactions', 'user'))
 						->with('title', 'Account')
 						->with('active', 'account-transactions');
 	}
+
+    public function create($userId)
+    {
+        $user = $this->user->find($userId);
+
+        $transactionTypes = $this->accountTransactionTypeRepository->findAll();
+
+        return View::make('admin::transactions.create', compact('user', 'transactionTypes'))
+            ->with('title', 'Account')
+            ->with('active', 'account-transactions');
+    }
+
+    public function store($userId)
+    {
+        $data = Input::all();
+
+        $data['amount'] = 100*$data['amount'];
+        $data['created_date'] = Carbon::now()->toDateTimeString();
+        $data['recipient_id'] = $userId;
+        $data['giver_id'] = \Auth::user()->id;
+        $data['session_tracking_id'] = -1;
+
+        $this->accountTransactionRepo->create($data);
+
+        return Redirect::route('admin.users.account-transactions.index', array($userId));
+    }
 
 }
