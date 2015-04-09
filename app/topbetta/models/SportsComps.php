fd153a097c1e58d80d83ea4b55852545f40c5656
Thomas Muir <thomas@topbetta.com>
2015-04-09 17:14:58 +1000

    Changed get sports and comps query to only get sports and comps with events which are not past their start date

--- app/topbetta/models/SportsComps.php
+++ app/topbetta/models/SportsComps.php
@@ -26,6 +26,9 @@ class SportsComps extends \Eloquent {
    public function getCompsSorted ($date = NULL, $sid = NULL){
     	//construct the date query string
     	$dateQuery = $this->mkDateQuery($date, 'eg.close_time');
+
+       //event date query
+       $eventDateQuery = $this->mkDateQuery($date, 'ev.start_date', "AND");
     	
     	//select sports if ids set
     	$sportQuery = ($sid) ? ' AND s.id IN ('.$sid.') ' : '';
@@ -33,6 +36,7 @@ class SportsComps extends \Eloquent {
     	$query = ' SELECT eg.name AS name, eg.created_date, eg.id AS eventGroupId, eg.start_date, eg.close_time';
     	$query .= ' FROM tbdb_event_group as eg';
 	    $query .= ' INNER JOIN tbdb_event_group_event AS ege ON ege.event_group_id = eg.id';
+        $query.= ' INNER JOIN tbdb_event AS ev ON ege.event_id = ev.id ';
 	    $query .= ' INNER JOIN tbdb_market AS m ON m.event_id = ege.event_id';
 	   	$query.= ' INNER JOIN tbdb_selection sel ON sel.market_id = m.id';
 	   	$query.= ' INNER JOIN tbdb_selection_price sp ON sel.id = sp.selection_id';
@@ -42,7 +46,9 @@ class SportsComps extends \Eloquent {
     	// $query .= ' LEFT JOIN tb_data_provider AS dp ON dp.id = dopm.provider_id ';
     	$query.= $dateQuery;
     	$query.= $sportQuery;
+        $query.= $eventDateQuery;
     	$query .= " AND eg.display_flag = '1'";
+        $query .= " AND ev.display_flag = '1' ";
 	   	$query .= " AND m.market_status NOT IN ('D', 'S') ";
 	    $query .= " AND sp.win_odds > 1";
 	    $query .= " AND sel.selection_status_id = '1'";
@@ -60,6 +66,9 @@ class SportsComps extends \Eloquent {
 			//construct the date query string
 			$dateQuery = $this->mkDateQuery($date, 'c.close_time');
 
+            //event date query
+            $eventDateQuery = $this->mkDateQuery($date, 'ev.start_date', "AND");
+
 			//select sports if ids set
 			//if ($sid) { $sportQuery = ' AND s.id IN ('.$sid.') ' ; }
 			$sportQuery = ($sid) ? ' AND s.id IN ('.$sid.') ' : '';
@@ -70,12 +79,15 @@ class SportsComps extends \Eloquent {
 			$query.= ' FROM tbdb_tournament_sport AS s ';
 			$query.= ' INNER JOIN tbdb_event_group AS c ON c.sport_id = s.id ';
 			$query.= ' INNER JOIN tbdb_event_group_event AS ege ON ege.event_group_id = c.id';
+            $query.= ' INNER JOIN tbdb_event AS ev ON ege.event_id = ev.id ';
 			$query.= ' INNER JOIN tbdb_market AS m ON m.event_id = ege.event_id';
 			$query.= ' INNER JOIN tbdb_selection sel ON sel.market_id = m.id';
 			$query.= ' INNER JOIN tbdb_selection_price sp ON sel.id = sp.selection_id';
 			$query.= $dateQuery;
 			$query.= $sportQuery;
+            $query.= $eventDateQuery;
 			$query .= " AND c.display_flag = '1' ";
+            $query .= " AND ev.display_flag = '1' ";
 			$query .= " AND m.market_status NOT IN ('D', 'S') ";
 			$query .= " AND sp.win_odds > 1";
 			$query .= " AND sel.selection_status_id = '1'";
@@ -102,18 +114,18 @@ class SportsComps extends \Eloquent {
 
 	}
 
-	private function mkDateQuery($date = NULL, $time_field) {
+	private function mkDateQuery($date = NULL, $time_field, $predicate = 'WHERE') {
 		if ($date && date('Y-m-d') != $date) {
 			if (strtotime($date) < time()) {
 				//date is in the past >> returns just on that date
-				$dateQuery = ' WHERE '.$time_field.' LIKE "'.$date.'%" ' ;
+				$dateQuery = " $predicate ".$time_field.' LIKE "'.$date.'%" ' ;
 			} else {
 				//date is in the future >> returns from date to future
-				$dateQuery = ' WHERE UNIX_TIMESTAMP('.$time_field.') > '.strtotime($date) ;
+				$dateQuery = " $predicate UNIX_TIMESTAMP(".$time_field.") > ".strtotime($date) ;
 			}
 		} else {
 			//no date or date is today >> returns from now to future
-			$dateQuery = ' WHERE UNIX_TIMESTAMP('.$time_field.') > '.time() ;
+			$dateQuery = " $predicate UNIX_TIMESTAMP(".$time_field.") > ".time() ;
 		}
 		return $dateQuery;
 	}
