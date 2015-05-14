4dcc31fc2de816e22cc495ab0d4489f5047b5bdd
Oliver Shanahan <oliver@topbetta.com>
2015-05-14 12:07:36 +1000

    remove array nesting

--- app/topbetta/Services/Accounting/AccountTransactionService.php
+++ app/topbetta/Services/Accounting/AccountTransactionService.php
@@ -212,7 +212,7 @@ class AccountTransactionService {
         }
 
         // return username and cents
-        return $returnArray = array($details);
+        return $returnArray = $details;
 
     }
 
