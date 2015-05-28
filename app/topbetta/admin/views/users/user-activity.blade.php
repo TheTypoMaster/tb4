4b5e20f70c30da01c7dde5b5ebe6d95a947a942e
Thomas Muir <thomas@topbetta.com>
2015-05-28 10:25:28 +1000

    Added example format to user activity view

--- app/topbetta/admin/views/users/user-activity.blade.php
+++ app/topbetta/admin/views/users/user-activity.blade.php
@@ -7,6 +7,10 @@
             <h2>User Activity</h2>
         </div>
 
+        <p>
+            Required CSV Format: first name, last name, DOB <br/><small>e.g. John, Smith, 01/01/2001</small>
+        </p>
+
         {{ Form::open(array('url' => 'admin/user-activity/download', 'method' => "POST", "files" => true)) }}
 
         <div class="form-group">
