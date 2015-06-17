f368e8b5e004df2527a342197f43c267cb8710e6
Oliver Shanahan <oliver@topbetta.com>
2015-06-17 10:35:49 +1000

    fix admin:: reference

--- resources/views/admin/transactions/create.blade.php
+++ resources/views/admin/transactions/create.blade.php
@@ -3,9 +3,9 @@
 @section('main')
     <div class="row">
         <div class="col-lg-12">
-            @include('admin::users.partials.header')
+            @include('admin.users.partials.header')
             <h2 class="page-header">{{ $title }} Transactions</h2>
-            @include('admin::transactions.partials.transaction-form')
+            @include('admin.transactions.partials.transaction-form')
         </div>
         <!-- /.col-lg-12 -->
     </div>
