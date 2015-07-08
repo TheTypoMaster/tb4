f6d7a760109391be9dfc409e9b77207ddcfc0358
Thomas Muir <thomas@topbetta.com>
2015-07-08 13:19:18 +1000

    Added v2 auth user route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -447,6 +447,9 @@ Route::group(array('prefix' => '/api/v2', 'after' => 'topbetta_secure_links'), f
     // normal logout
     Route::get('authentication/logout', 'Frontend\UserSessionController@logout');
 
+    //Get auth user
+    Route::get('authentication/user', 'Frontend\UserSessionController@user');
+
     // token login
     Route::get('authentication/token/login', 'Frontend\UserTokenController@tokenLogin');
 
