96a1a5894b460a3215143b06b696dac8650671b5
Thomas Muir <thomas@topbetta.com>
2015-08-18 16:31:54 +1000

    updated exposure bet limits seeder

--- database/seeds/tbdb_bet_limit_types_add_exposure_limits.php
+++ database/seeds/tbdb_bet_limit_types_add_exposure_limits.php
@@ -13,21 +13,21 @@ class tbdb_bet_limit_types_add_exposure_limits extends Seeder
     {
         $tbdb_bet_limit_add_exposure_limits = array(
             array(
-                "name"           => "exposure_racing",
+                "name"           => "exposure",
                 "nickname"       => "Win Exposure",
                 "value"          => "1",
                 "default_amount" => 100000,
                 "notes"          => "Win bets exposure limit",
             ),
             array(
-                "name"           => "exposure_racing",
+                "name"           => "exposure",
                 "nickname"       => "Place Exposure",
                 "value"          => "2",
                 "default_amount" => 100000,
                 "notes"          => "Place bets exposure limit",
             ),
             array(
-                "name"           => "exposure_sport",
+                "name"           => "exposure",
                 "nickname"       => "Sport Exposure",
                 "value"          => 17,
                 "default_amount" => 100000,
