9ef814a2b1868af8149ae029392c53902a3320f9
Thomas Muir <thomas@topbetta.com>
2015-08-31 16:14:50 +1000

    Products for tournaments now set to be default

---
+++ app/Services/Resources/Tournaments/TournamentMeetingResourceService.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Thomas Muir
+ * Date: 31/08/2015
+ * Time: 3:08 PM
+ */
+
+namespace TopBetta\Services\Resources\Tournaments;
+
+
+use TopBetta\Resources\MeetingResource;
+use TopBetta\Services\Resources\MeetingResourceService;
+
+class TournamentMeetingResourceService extends MeetingResourceService {
+
+    public function loadTotesForMeeting(MeetingResource $meetingResource)
+    {
+        $products = $meetingResource->getModel()->products;
+
+        foreach ($meetingResource->races as $race) {
+            $race->setProducts($products);
+        }
+
+        return $meetingResource;
+    }
+}
