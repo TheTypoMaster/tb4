bcaf99a5687f679a29faaff9a19bd6d080cd1c78
Thomas Muir <thomas@topbetta.com>
2015-09-04 09:17:39 +1000

    Added products relation to sports comps model

--- app/Models/SportsComps.php
+++ app/Models/SportsComps.php
@@ -25,6 +25,14 @@ class SportsComps extends \Eloquent {
     	return SportsComps::where('name', '=', $compName) -> value('id');
     }
 
+    public function products()
+    {
+        return $this->belongsToMany('TopBetta\Models\BetProductModel', 'tb_default_event_group_product', 'event_group_id', 'bet_product_id')
+            ->join('tbdb_bet_type', 'tbdb_bet_type.id', '=', 'tb_default_event_group_product.bet_type_id')
+            ->join('tb_product_provider_match', 'tb_product_provider_match.tb_product_id', '=', 'tbdb_bet_product.id')
+            ->select(array('tbdb_bet_product.*', 'tbdb_bet_type.name as bet_type', 'tb_product_provider_match.provider_product_name as product_code'));
+    }
+
    public function getCompsSorted ($date = NULL, $sid = NULL){
     	//construct the date query string
     	$dateQuery = $this->mkDateQuery($date, 'eg.close_time');
