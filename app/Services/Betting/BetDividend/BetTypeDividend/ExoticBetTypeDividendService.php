1c80eff6777a4234e9e71d3b9b4246c6d336f600
Thomas Muir <thomas@topbetta.com>
2015-09-10 20:07:39 +1000

    Fixed exotic bet resulting

--- app/Services/Betting/BetDividend/BetTypeDividend/ExoticBetTypeDividendService.php
+++ app/Services/Betting/BetDividend/BetTypeDividend/ExoticBetTypeDividendService.php
@@ -29,7 +29,7 @@ class ExoticBetTypeDividendService extends AbstractBetTypeDividendService {
             $result = explode('/', $dividend->result_string);
 
             //boxed so just check selections contained in $result
-            if( $bet->boxed_flag && count(array_intersect($bet->selection->lists('number')->all(), $result)) == count($bet->selection->lists('number')->all())) {
+            if( $bet->boxed_flag && count(array_intersect($result, $bet->selection->lists('number')->all())) == count($result)) {
                 $totalDividend += $dividend->dividend;
             } else {
                 $hasSelections = true;
