a3625bb8a6057c81db2f69a3f4ef3de1319b1e07
Oliver Shanahan <oliver@topbetta.com>
2015-09-15 15:22:56 +1000

    put result on queue

---
+++ app/Services/Feeds/Queues/RiskManagerPushAPIService.php
@@ -0,0 +1,41 @@
+<?php namespace TopBetta\Services\Feeds\Queues;
+
+/**
+ * Coded by Oliver Shanahan
+ * File creation date: 15/09/15
+ * File creation time: 09:06
+ * Project: tb4
+ */
+
+use Guzzle\Http\Client;
+use Guzzle\Http\Exception\ServerErrorResponseException as GuzzleException;
+use Guzzle\Http\Exception\CurlException as GuzzleCurlException;
+use Config;
+use Log;
+
+
+class RiskManagerPushAPIService
+{
+    public function pushDataToRiskManagerAPI($data)
+    {
+
+        // send bet to risk manager
+        $responseJSON = CurlRequestHelper::curlRequest(Config::get('riskmanager.RISK_FEED_API'),
+            Config::get('riskmanager.RISK_RACE_DATA_URI'),
+            'POST',
+            json_encode($data));
+
+        $response = json_decode($responseJSON);
+
+        if (!$response) {
+            return false;
+        }
+
+        Log::debug('RiskManagerAPI (sendResultData): Response - '.print_r($response,true));
+        if ($response->http_status_code == 200) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
