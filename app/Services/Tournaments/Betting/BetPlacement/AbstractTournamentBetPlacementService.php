6ebdbbbf6d43dc05bff1a30ec575150e71d55c18
Thomas Muir <thomas@topbetta.com>
2015-09-16 12:32:30 +1000

    Fixed turned_over balance update on tournament bet

--- app/Services/Tournaments/Betting/BetPlacement/AbstractTournamentBetPlacementService.php
+++ app/Services/Tournaments/Betting/BetPlacement/AbstractTournamentBetPlacementService.php
@@ -16,6 +16,7 @@ use TopBetta\Services\Betting\Exceptions\BetSelectionException;
 use TopBetta\Services\Betting\Factories\BetSelectionFactory;
 use TopBetta\Services\Tournaments\Betting\TournamentBetLimitService;
 use TopBetta\Services\Tournaments\TournamentBetSelectionService;
+use TopBetta\Services\Tournaments\TournamentLeaderboardService;
 use TopBetta\Services\Tournaments\TournamentTicketService;
 
 abstract class AbstractTournamentBetPlacementService {
@@ -50,14 +51,19 @@ abstract class AbstractTournamentBetPlacementService {
      * @var BetResultStatusRepositoryInterface
      */
     private $betResultStatusRepository;
+    /**
+     * @var TournamentLeaderboardService
+     */
+    private $leaderboardService;
 
-    public function __construct(TournamentBetRepositoryInterface $betRepository, TournamentBetLimitService $betLimitService, TournamentTicketService $ticketService, BetResultStatusRepositoryInterface $betResultStatusRepository)
+    public function __construct(TournamentBetRepositoryInterface $betRepository, TournamentBetLimitService $betLimitService, TournamentTicketService $ticketService, BetResultStatusRepositoryInterface $betResultStatusRepository, TournamentLeaderboardService $leaderboardService)
     {
         $this->betRepository = $betRepository;
         $this->selectionService = new TournamentBetSelectionService(\App::make($this->selectionServiceClass));
         $this->betLimitService = $betLimitService;
         $this->ticketService = $ticketService;
         $this->betResultStatusRepository = $betResultStatusRepository;
+        $this->leaderboardService = $leaderboardService;
     }
 
     public function placeBet($ticket, $selections, $amount, $betType)
@@ -106,6 +112,8 @@ abstract class AbstractTournamentBetPlacementService {
             $this->betRepository->deleteById($bet['id']);
             throw new BetSelectionException(null, "Error creating bet selections");
         }
+
+        $this->leaderboardService->increaseTurnedOver($ticket->tournament_id, $ticket->user_id, $amount);
 
         return $bet;
     }
