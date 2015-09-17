29491b2903386ed1f8485a64b7a9e70e964364cd
Oliver Shanahan <oliver@topbetta.com>
2015-09-17 13:40:18 +1000

    tournament type

--- app/Services/Tournaments/TournamentService.php
+++ app/Services/Tournaments/TournamentService.php
@@ -342,12 +342,6 @@ class TournamentService {
             $tournamentData['start_date'] = array_get($tournamentData, 'future_meeting_date');
             $tournamentData['end_date'] = $tournamentData['start_date'];
 
-            if($tournamentData['tournament_sport_id'] == 1){
-                $tournamentData['tournament_type'] = 'Racing';
-            }else{
-                $tournamentData['tournament_type'] = 'Sport';
-            }
-
             //betting closed date
             if (array_get($tournamentData, 'close_betting_on_first_match_flag')) {
                 $tournamentData['betting_closed_date'] = $tournamentData['start_date'];
@@ -369,6 +363,12 @@ class TournamentService {
             } else {
                 $tournamentData['betting_closed_date'] = $tournamentData['end_date'];
             }
+        }
+
+        if($tournamentData['tournament_sport_id'] == 1){
+            $tournamentData['tournament_type'] = 'Racing';
+        }else{
+            $tournamentData['tournament_type'] = 'Sport';
         }
 
         //tournament of the day
