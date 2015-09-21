6d0127cbf3890635b37d3f62deb06dc9efd8f2f7
Thomas Muir <thomas@topbetta.com>
2015-09-21 19:02:03 +1000

    Fixed form on selection

--- app/Resources/SelectionResource.php
+++ app/Resources/SelectionResource.php
@@ -98,6 +98,10 @@ class SelectionResource extends AbstractEloquentResource {
 
     public function getForm()
     {
+        if ($form = object_get($this->model, 'form')) {
+            return $form;
+        }
+
         return $this->model->last_starts;
     }
 
