b4f393ad60a451bf74bd2c4ae571b3838c0b10aa
Jialei <Jialei@topbetta.com>
2015-09-30 15:23:15 +1000

    fix competition errors caused by using shared templates

--- resources/views/admin/eventdata/markettypes/index.blade.php
+++ resources/views/admin/eventdata/markettypes/index.blade.php
@@ -1,6 +1,97 @@
 @extends('admin.layouts.master')
 
 @section('main')
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="row page-header">
+                <h2 class="col-lg-4">
+                    {{ $data['modelName'] }} <small>({{ number_format($data['modelCollection']->total()) }})</small>
+                    <a href="{{route($data['createRoute'], array("q" => $data['search']))}}" class="btn btn-primary">Create <i class="glyphicon glyphicon-plus"></i></a>
+                </h2>
 
-    @include('admin.eventdata.partials.templates.index-template', $data);
+                <h2 class="col-lg-4 pull-right">
+                    {!! Form::open(array('method' => 'GET')) !!}
+                    <div class="input-group custom-search-form">
+                        {!! Form::text('q', $data['search'], array("class" => "form-control", "placeholder" => "Search...")) !!}
+                        <span class="input-group-btn">
+                    <button class="btn btn-default" type="button">
+                        <i class="fa fa-search"></i>
+                    </button>
+                </span>
+                    </div>
+                    {!! Form::close() !!}
+                </h2>
+            </div>
+
+            @if (count($data['modelCollection']))
+                <table class="table table-striped table-bordered table-hover">
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Icon</th>
+                        <th>Name</th>
+                        <th>Short Name</th>
+                        <th>Default Name</th>
+                        <th>Description</th>
+                        <th>Display</th>
+                        @foreach($data['extraFields'] as $header=>$field)
+                            <th>{{ $header }}</th>
+                        @endforeach
+                        <th>Group</th>
+                        <th>Created</th>
+                        <th>Updated</th>
+                        <th colspan="1">Action</th>
+                    </tr>
+                    </thead>
+
+                    <tbody>
+                    @foreach($data['modelCollection'] as $model)
+                        <tr>
+                            <td>{{ $model->id }}</td>
+                            <td>
+                                @if($model->icon)
+                                    <img src="{{ $model->icon->icon_url }}" style="width:25px;height:25px;">
+                                @endif
+                            </td>
+                            <td>{{ $model->name }}</td>
+                            <td>{{ $model->short_name }}</td>
+                            <td>{{ $model->default_name }}</td>
+                            <td>{{ $model->description }}</td>
+                            <td>{{ $model->display_flag ? "Yes" : "No" }}</td>
+
+                            @foreach($data['extraFields'] as $field)
+                                <td>
+                                    @if($field['type'] == 'image')
+                                        <img src="{{ object_get($model, $field['field']) }}" />
+                                    @elseif($field['type'] == 'closure')
+                                        {{ call_user_func($field['field'], $model) }}
+                                    @else
+                                        {{ object_get($model, $field['field']) }}
+                                    @endif
+                                </td>
+                            @endforeach
+                            <td>{{ $model->markettypegroup->market_type_group_name }}</td>
+                            <td>{{ $model->created_at }}</td>
+                            <td>{{ $model->updated_at }}</td>
+                            <td>
+                                @if ( $data['createChildRoute'] )
+                                    {!! link_to_route($data['createChildRoute']['route'], 'Create' . $data['createChildRoute']['name'], array('q' => $data['search'], $data['createChildRoute']['param'] => $model->id), array('class' => 'btn btn-primary') ) !!}
+                                @endif
+                                <a href="{{ URL::route($data['editRoute'], array($model->id, "q" => $data['search'])) }}" class="btn btn-warning"><i class="glyphicon glyphicon-edit"></i> Edit</a>
+
+                                {!! Form::open(array("method" => "DELETE", "route"=>array($data['deleteRoute'], $model->id))) !!}
+                                <button type="submit" class="btn btn-danger delete-button"><i class="glyphicon glyphicon-remove"></i> Delete</button>
+                                {!! Form::close() !!}
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+                {!! $data['modelCollection']->appends(array('q' => $data['search']))->render() !!}
+            @else
+                <p>There is nothing to display</p>
+            @endif
+        </div>
+    </div>
+
 @stop
--- resources/views/admin/eventdata/partials/templates/form-template.blade.php
+++ resources/views/admin/eventdata/partials/templates/form-template.blade.php
@@ -82,16 +82,19 @@
                 </div>
             @endforeach
 
-            @if(isset($model)))
-            <div class="form-group">
-                {!! Form::label('market_type_group_id', 'Group: ') !!}
-                {!! Form::select('market_type_group_id', $extraFields["Market Rules"]["market_type_group_list"], $model->market_type_group_id, array('id' => 'groups', 'class' => 'form-control')) !!}
-            </div>
-                @else
-                {!! Form::label('market_type_group_id', 'Group: ') !!}
-                {!! Form::select('market_type_group_id', $extraFields["Market Rules"]["market_type_group_list"], '', array('id' => 'groups', 'class' => 'form-control')) !!}
-            @endif
-        </div>
+            {{--@section('market-type-group')--}}
+                {{--@stop--}}
+        @yield('market-type-group')
+        {{--@if(isset($model)))--}}
+            {{--<div class="form-group">--}}
+                {{--{!! Form::label('market_type_group_id', 'Group: ') !!}--}}
+                {{--{!! Form::select('market_type_group_id', $extraFields["Market Rules"]["market_type_group_list"], $model->market_type_group_id, array('id' => 'groups', 'class' => 'form-control')) !!}--}}
+            {{--</div>--}}
+                {{--@else--}}
+                {{--{!! Form::label('market_type_group_id', 'Group: ') !!}--}}
+                {{--{!! Form::select('market_type_group_id', $extraFields["Market Rules"]["market_type_group_list"], '', array('id' => 'groups', 'class' => 'form-control')) !!}--}}
+            {{--@endif--}}
+        {{--</div>--}}
 
 
         <div class="col-lg-12">
